f42fac12472ebc12a57dfa3fab6d544af8fe6746
aakashdeveloper <ahanda205@gmail.com>
2021-06-28 06:52:22 +0530

    expressRouting

--- fullstack/app.js
+++ fullstack/app.js
@@ -1,18 +1,24 @@
 var express = require('express');
 var app = express();
-var port = 5600;
+var dotenv = require('dotenv');
+dotenv.config();
+var fs = require('fs');
+var port = process.env.PORT || 5600;
+var hotelRouter = require('./src/router/hotelRoutes')
+var cityRouter = require('./src/router/cityRouters');
+var morgan = require('morgan');
+
+// middleware
+app.use(morgan('combined'))
+
 
 app.get('/',function(req,res){
     res.send("Hii From express");
 })
 
-app.get('/city',function(req,res){
-    res.send("Hiii from city");
-})
+app.use('/hotel',hotelRouter);
+app.use('/city', cityRouter);
 
-app.get('/hotels',function(req,res){
-    res.send("Hiii from hotels page");
-})
 
 app.listen(port, function(err){
     if(err) throw err;
